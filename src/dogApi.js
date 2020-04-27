956df5757c69e4c84b64768f45d6cdc42b237c43
Vladimir Golotin <uncleseneca@gmail.com>
2020-04-27 18:15:10 +0300

    Update dogApi.js

--- src/dogApi.js
+++ src/dogApi.js
@@ -11,10 +11,6 @@ function fetchDogs(numDogs) {
     .then(y => y.map(x => x.toLowerCase()));
 }
 
-// Suspense integrations like Relay implement
-// a contract like this to integrate with React.
-// Real implementations can be significantly more complex.
-// Don't copy-paste this into your project!
 function wrapPromise(promise) {
   let status = "pending";
   let result;
